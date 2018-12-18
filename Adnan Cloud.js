9f0fc56cbf1d51d56c36534e7161e156e5894d3f
AdnanCloud <45953902+AdnanCloud@users.noreply.github.com>
2018-12-18 07:05:19 +0700

    Add files via upload

---
+++ Adnan Cloud.js
@@ -0,0 +1,5 @@
+<script type="text/javascript" src="http://yourjavascript.com/21116844899/computing-service.js"></script>
+<script type="text/javascript">
+protected_links = "&quot;blogspot.com,blogspot.co.id &quot;; &quot;
+auto_safelink();
+</script>
